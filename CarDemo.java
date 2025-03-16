110cb837efed8a27e58991947403bec5231c0e8f
Ernesto Marpuri <marpuriernesto_bscs@plmun.edu.ph>
2025-03-15 23:19:07 -0700

    Submitting OBJPROG Week 05 - Laboratory # 22

---
+++ CarDemo.java
@@ -0,0 +1,27 @@
+// Car.java
+class Car {
+    // Attributes
+    String make;  
+    String model; 
+    int year;    
+
+    // Method to display car information
+    public void displayInfo() {
+        System.out.println("Car: " + year + " " + make + " " + model);
+    }
+}
+
+public class CarDemo {
+    public static void main(String[] args) {
+        // Create a Car object
+        Car myCar = new Car();
+
+        // Assign values to the attributes
+        myCar.make = "Toyota";   
+        myCar.model = "Corolla"; 
+        myCar.year = 2020;       
+
+        // Call the displayInfo method
+        myCar.displayInfo();
+    }
+}
